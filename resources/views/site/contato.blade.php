3c336e7a5beb764e37a64c239a6285484cdd76cd
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2021-07-08 09:31:42 -0300

    Ajustar

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -13,25 +13,9 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    <form action="{{  route('site.contato') }}" method="post" >
-                        @csrf
-                        <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                        <br>
-                        <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                        <br>
-                        <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                        <br>
-                        <select name="motivo_contato" class="borda-preta">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="">Dúvida</option>
-                            <option value="">Elogio</option>
-                            <option value="">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
+                    @component('site.layouts._components.form_contato')
+                    <p>A nossa equipe analisará a sua mensagem para brevemente atende-lo</p>
+                    @endcomponent
                 </div>
             </div>
         </div>
