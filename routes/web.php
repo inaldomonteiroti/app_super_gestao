cfd4e4560f4ce789614bbba2d66c83934ea9e94a
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2021-11-02 18:44:23 -0300

    Ajustando

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PrincipalController@principal')->name('site.principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
@@ -25,5 +25,5 @@ Route::prefix('/app')->group(function(){
 });
 
 Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+    echo 'A rota acessada não existe. <a href="'.route('site.principal').'">clique aqui</a> para ir para página inicial';
 });
