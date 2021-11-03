a6406bae45567e3f95563b2de753ca01d8fdf3d1
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2021-11-03 16:31:08 -0300

    Ajustando

--- database/migrations/2021_11_03_192128_create_produtos_table.php
+++ database/migrations/2021_11_03_192128_create_produtos_table.php
@@ -20,7 +20,7 @@ class CreateProdutosTable extends Migration
             $table->integer('peso')->nullable();
             $table->float('preco_venda', 8,2)->default(0,01);
             $table->integer('estoque_minimo')->default(1);
-            $table->string('estoque_maximo')->default('1');
+            $table->integer('estoque_maximo')->default(1);
             $table->timestamps();
         });
     }
